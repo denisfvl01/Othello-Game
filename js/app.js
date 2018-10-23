fb95071cdab003e262bda208f3590c60cff1e3ce
Bryan de Paz <bryanascuc7@gmail.com>
2018-10-23 11:05:06 -0600

    Verificacion de turnos

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 document.addEventListener("keydown", movimiento);
 var canvas = document.getElementById('tablero');
 var lapiz = canvas.getContext('2d');
+var matriz = new Array(8);
 const ESPACIOX = 8;
 const ESPACIOY = 9;
-// const 
+var turnoNegro = false; 
 
 var tablero = {
     url: './imagenes/tablero.png',
@@ -12,13 +13,18 @@ var tablero = {
 };
 var FichaBlanca = {
     url: './imagenes/FichaBlanca.png',
-    imagen: Image,
-    cargaOk: false
+    imagen: Image
 };
-var FichaNegra ={
+var FichaNegra = {
     url: './imagenes/FichaNegra.png',
-    imagen: Image,
-    cargaOk: false
+    imagen: Image
+}
+var tecla = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+    ENTER: 13
 }
 
 tablero.imagen = new Image();
@@ -35,34 +41,56 @@ tablero.imagen.addEventListener("load", function() {
     dibujar();
 });
 
-FichaBlanca.imagen.addEventListener("load", function() {
-    FichaBlanca.cargaOk = true;
-    dibujar();
-});
-FichaNegra.imagen.addEventListener("load", function(){
-    FichaNegra.cargaOk = true;
-    dibujar();
-});
+generarMatriz();
+generarFichasNegras();
+generarFichasBlancas();
+dibujar();
 
 function dibujar() {
     if (tablero.cargaOk) {
         lapiz.drawImage(tablero.imagen, 0, 0);
     }
-    if (FichaBlanca.cargaOk) {
-        lapiz.drawImage(FichaBlanca.imagen, 240+ESPACIOX, 180+ESPACIOY);
-        lapiz.drawImage(FichaBlanca.imagen,180+ESPACIOX, 240+ESPACIOY);
+};
+
+function generarMatriz() {
+    for (let row = 0; row < matriz.length; row++) {
+        matriz[row] = new Array(8);
+        for (let col = 0; col < matriz[row].length; col++) {
+            matriz[row][col] = 'x';
+        }
     }
-    if (FichaNegra.cargaOk){
-        lapiz.drawImage(FichaNegra.imagen, 240+ESPACIOX,240+ESPACIOY);
-        lapiz.drawImage(FichaNegra.imagen, 180+ESPACIOX,180+ESPACIOY);
+};
+
+function generarFichasNegras() {
+    matriz[180 / 60][180 / 60] = 'ficha-negra';
+    matriz[240 / 60][240 / 60] = 'ficha-negra';
+};
+
+function generarFichasBlancas() {
+    matriz[180 / 60][240 / 60] = 'ficha-blanca';
+    matriz[240 / 60][180 / 60] = 'ficha-blanca';
+};
+
+function dibujarMatriz() {
+    for (let row = 0; row < matriz.length; row++) {
+        for (let col = 0; col < matriz.length; col++) {
+            if (matriz[row][col] == 'ficha-negra') {
+                lapiz.drawImage(FichaNegra.imagen, (60 * col) + ESPACIOX, (60 * row) + ESPACIOY);
+            }
+            if (matriz[row][col] == 'ficha-blanca') {
+                lapiz.drawImage(FichaBlanca.imagen, (60 * col) + ESPACIOX, (60 * row) + ESPACIOY);
+            }
+        }
     }
-}
-var tecla = {
-    LEFT: 37,
-    UP: 38,
-    RIGHT: 39,
-    DOWN: 40,
-    ENTER: 13
+};
+
+function nuevoJuego() {
+    turnoNegro = true;
+    generarMatriz();
+    generarFichasNegras();
+    generarFichasBlancas();
+    dibujarMatriz();
+    turnos();
 }
 
 function movimiento(evento) {
@@ -84,4 +112,13 @@ function movimiento(evento) {
     //         alert('Enter');
     //         break;
     // }
+}
+function turnos(){
+ if (turnoNegro){
+     alert('Mueve ficha negra');
+     //turnoNegro();
+ }else{
+     alert('Mueve ficha blanca');
+     //turnoBlanco();
+ }
 }
